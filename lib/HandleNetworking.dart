29bea108e375913de24e08b1fbd197cd92b1c2a7
Jatin6256 <sachdevaj6256@gmail.com>
2020-12-14 21:22:33 +0530

    started connection with backend

--- lib/HandleNetworking.dart
+++ lib/HandleNetworking.dart
@@ -4,6 +4,8 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:professor_interface/Models/FutureResponse.dart';
+import 'package:professor_interface/components/CourseTile.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HandleNetworking
 {
@@ -51,6 +53,57 @@ class HandleNetworking
       return null;
 
 
-
   }
+
+    Future<List<CourseTile>> getCourses() async
+    {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String profId = prefs.getString('id');
+      final http.Response response = await http.get("https://lit-reef-57031.herokuapp.com/api/course/getcourses/" + profId);
+
+      if(response.statusCode == 200)
+        {
+            var jsonRes = jsonDecode(response.body);
+            List courses = jsonRes['result'];
+            print(courses);
+            List<CourseTile> coursesTileList = [];
+
+            for (int i = 0;i < courses.length;i++)
+            {
+              coursesTileList.add(CourseTile("Todo", courses[i]['name']));
+            }
+
+            return coursesTileList;
+
+        }
+        else
+          {
+            return null;
+          }
+
+    }
+
+    Future<bool> addCourse(String courseName,String courseId) async
+    {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String profId = prefs.getString('id');
+
+      final http.Response response = await http.post(
+          "https://lit-reef-57031.herokuapp.com/api/course/create",
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: jsonEncode(<String, dynamic>{
+
+            'name' : courseName,
+            'admin_id': profId,
+          })
+
+      );
+
+      if(response.statusCode == 200)
+        return true;
+      else
+        return false;
+    }
 }
--- lib/screens/coursesList.dart
+++ lib/screens/coursesList.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:professor_interface/HandleNetworking.dart';
 import 'package:professor_interface/components/CourseTile.dart';
 import 'package:professor_interface/screens/AddCourses.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class CoursesList extends StatelessWidget {
+class CoursesList extends StatefulWidget {
+  @override
+  _CoursesListState createState() => _CoursesListState();
+}
+
+class _CoursesListState extends State<CoursesList> {
+
+
+
   Future<void> _showMyDialog(BuildContext context) async {
     return showDialog<void>(
       context: context,
@@ -43,6 +52,21 @@ class CoursesList extends StatelessWidget {
     );
   }
 
+
+
+    Future<List<CourseTile>> coursesList;
+    void fetchCoursesList() async
+    {
+      HandleNetworking handleNetworking = HandleNetworking();
+      coursesList =  handleNetworking.getCourses();
+    }
+
+    @override
+  void initState() {
+    super.initState();
+    fetchCoursesList();
+
+  }
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -50,6 +74,17 @@ class CoursesList extends StatelessWidget {
         backgroundColor: Colors.black,
         title: Text('AMS'),
         actions: [
+          IconButton(
+              icon: Icon(
+                Icons.refresh,
+                color: Colors.white,
+              ),
+              onPressed: (){
+                fetchCoursesList();
+                setState(() {
+                  
+                });
+              }),
           IconButton(
         icon: Icon(
         Icons.power_settings_new,
@@ -60,24 +95,38 @@ class CoursesList extends StatelessWidget {
           })
         ],
       ),
-       body: ListView(
-          children: [
-            CourseTile("CS123","Data Structure"),
-          ],
-        ),
+       body: FutureBuilder<List<CourseTile>>(
+         future: coursesList,
+         builder: (context, snapshot) {
+           if (snapshot.hasData) {
+             return ListView(
+               children: snapshot.data,
+             );
+           } else if (snapshot.hasError) {
+             return Text("${snapshot.error}");
+           }
+
+           return Center(child: CircularProgressIndicator());
+         },
+       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-          showModalBottomSheet(
-              context: context,
-              isScrollControlled: true,
-              builder: (context) => SingleChildScrollView(
-                  child:Container(
-                    padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-                    child: AddCourses(),
-                  )
-              )
+          // showModalBottomSheet(
+          //     context: context,
+          //     isScrollControlled: true,
+          //     builder: (context) => SingleChildScrollView(
+          //         child:Container(
+          //           padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          //           child: AddCourses(),
+          //         )
+          //     )
+          // );
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AddCourses())
           );
         },
+
         child: Icon(
           Icons.add,
           color: Colors.white,
